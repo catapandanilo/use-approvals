4a0d5f6439d41aea819a8779c5713f7152d5d8f4
Danilo <danilo.foton@foton.la>
2021-08-03 21:58:22 -0300

    refactor: return an object from hello endpoint

--- src/main/java/org/acme/ExampleResource.java
+++ src/main/java/org/acme/ExampleResource.java
@@ -4,13 +4,15 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/hello")
 public class ExampleResource {
 
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello RESTEasy";
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response hello() {
+        return Response.ok("{\"message\" : \"Hello Approvals\"}").build();
     }
+
 }
