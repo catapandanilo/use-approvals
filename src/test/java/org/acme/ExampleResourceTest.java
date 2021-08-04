a2f514625711c4e0a74ad97deff42895a610de5f
Danilo <danilo.foton@foton.la>
2021-08-03 22:01:17 -0300

    test: use Approvals to valid hello endpoint

--- src/test/java/org/acme/ExampleResourceTest.java
+++ src/test/java/org/acme/ExampleResourceTest.java
@@ -1,21 +1,21 @@
 package org.acme;
 
 import io.quarkus.test.junit.QuarkusTest;
+import org.approvaltests.Approvals;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
-public class ExampleResourceTest {
+class ExampleResourceTest {
 
     @Test
-    public void testHelloEndpoint() {
-        given()
+    void testHelloEndpoint() {
+        String result = given()
           .when().get("/hello")
-          .then()
-             .statusCode(200)
-             .body(is("Hello RESTEasy"));
+          .then().extract().asString();
+
+        Approvals.verifyJson(result);
     }
 
 }
