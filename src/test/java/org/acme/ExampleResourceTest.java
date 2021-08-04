ac8f10eb69fd585c4e0bbc277dbf88d4c0c8e320
Danilo <danilo.foton@foton.la>
2021-08-03 22:41:29 -0300

    test: use Approvals.verify and Approvals.verifyAll

--- src/test/java/org/acme/ExampleResourceTest.java
+++ src/test/java/org/acme/ExampleResourceTest.java
@@ -1,8 +1,11 @@
 package org.acme;
 
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.response.Response;
 import org.approvaltests.Approvals;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
 
 import static io.restassured.RestAssured.given;
 
@@ -16,6 +19,23 @@ class ExampleResourceTest {
           .then().extract().asString();
 
         Approvals.verifyJson(result);
+    }
+
+    @Test
+    void testHelloObjectEndpoint() {
+        String result = given()
+                .when().get("/hello/object")
+                .then().extract().asString();
+
+        Approvals.verify(result);
+    }
+
+    @Test
+    void testHelloObjectAndStatusEndpoint() {
+        Response response = given()
+                .when().get("/hello/object");
+
+        Approvals.verifyAll("", Arrays.asList(response.getStatusCode(), response.getBody().asString()));
     }
 
 }
